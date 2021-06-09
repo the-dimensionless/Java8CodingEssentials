0843cef1d0e5c8634eacc382a1c1db666e82a421
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2021-06-10 03:43:42 +0530

    flatMap() to flatten nested lists

--- src/com/spider/streams_api/ReduceAndFlatMap.java
+++ src/com/spider/streams_api/ReduceAndFlatMap.java
@@ -1,6 +1,9 @@
 package com.spider.streams_api;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReduceAndFlatMap {
     public static void main(String[] args) {
@@ -12,5 +15,20 @@ public class ReduceAndFlatMap {
         Integer reduce2 = Arrays.stream(arr)
                 .reduce(0, Integer::sum);
         System.out.println("reduced value is " + reduce+" or "+reduce2);
+
+        // FlatMap
+        List<List<String>> list = new ArrayList<>();
+        list.add(new ArrayList<>(Arrays.asList("A", "B", "C")));
+        list.add(new ArrayList<>(Arrays.asList("1", "2", "3")));
+        list.add(new ArrayList<>(Arrays.asList("1A", "2B", "3C")));
+        list.add(new ArrayList<>(Arrays.asList("A1", "B2", "C3")));
+
+        System.out.println("before flatmap -> " + list);
+
+        List<String> collect = list.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        System.out.println("after flatmap -> " + collect);
     }
 }
